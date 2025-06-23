93ad043dd6432f513d35d1ef934dfd8724736523
mona.abdelmalek <mona.abdelmalek@cyshield.com>
2025-06-23 15:08:45 +0300

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Odibee_Sans } from "next/font/google";
+const odibeeSans = Odibee_Sans({
+  variable: "--font-odibee-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${odibeeSans.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import DetailsSction from "@/components/DetailsSction";
+// import DetailsSction from "@/components/DetailsSction";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <DetailsSction/>     
+      {/* <DetailsSction/>      */}
     </>
   );
 }
